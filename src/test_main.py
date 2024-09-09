0b38128155cfbc5f9a143d219cab4ba9e7eeecf4
Danial Kurtumerov <kur7umerovd@gmail.com>
2024-09-09 12:54:05 +0500

    tests: edge cases
    Added tests for edge cases.

--- src/test_main.py
+++ src/test_main.py
@@ -1,20 +1,69 @@
 """Tests."""
 
+from fastapi.testclient import TestClient
+from httpx import Response
 from schemathesis import experimental, models
 from schemathesis.specs.openapi import loaders, schemas
 
+from main import Response as APIResponse
 from main import app
 
 experimental.OPEN_API_3_1.enable()
 
+test_client: TestClient = TestClient(app)
 schema: schemas.BaseOpenAPISchema = loaders.from_asgi('/openapi.json', app)
 
 
 @schema.parametrize()
-def test_schemathesis(case: models.Case):
+def test_schemathesis(case: models.Case) -> None:
     """Schemathesis tests.
 
     Args:
         case (models.Case): Schemathesis injection.
     """
     case.call_and_validate()
+
+
+def _check(before: str, after: str | None = None) -> None:
+    success_code: int = 200
+    response: Response = test_client.post('/', json={'text': before})
+
+    if response.status_code != success_code:
+        raise ValueError('Failed')
+
+    if after:
+        response_data: APIResponse = APIResponse(**response.json())
+        if response_data.text != after:
+            raise ValueError()
+
+
+def test_very_long_text() -> None:
+    """Test very long text."""
+    power: int = 16
+    text_length: int = 2 ** power - 1
+    very_long_text: str = 'a' * text_length
+    _check(very_long_text)
+
+
+def test_sentence_with_terminal() -> None:
+    """Test text with terminal punctuation marks."""
+    text_with_terminal: str = 'aaa... a, aa'
+    _check(text_with_terminal, 'aaa...')
+
+
+def test_sentence_with_internal() -> None:
+    """Test text with internal punctuation marks."""
+    text_with_internal: str = 'aaa, a, aa'
+    _check(text_with_internal, 'aaa, a,')
+
+
+def test_sentence_with_spaces() -> None:
+    """Test text with spaces only."""
+    text_with_spaces: str = 'aaa a aa'
+    _check(text_with_spaces, 'aaa a')
+
+
+def test_sentence_without_spaces() -> None:
+    """Test text without spaces and any punctuation marks."""
+    text_without_spaces: str = 'aaaaa'
+    _check(text_without_spaces, 'aaaaa')
