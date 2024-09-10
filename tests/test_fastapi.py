983d71fdbb22831f14faa76c63b90ede743504ff
Danial Kurtumerov <kur7umerovd@gmail.com>
2024-09-10 14:45:15 +0500

    tests: updated tests
    Updated tests to catch bug when the vary last part
    of text was ignored.

--- tests/test_fastapi.py
+++ tests/test_fastapi.py
@@ -6,6 +6,7 @@ from src.main import Settings, app
 from tests.utils_for_tests import check
 
 test_client: TestClient = TestClient(app)
+done_true: str = r'{"text":"Done","done":true}\n'
 
 
 def test_very_long_text() -> None:
@@ -17,38 +18,62 @@ def test_very_long_text() -> None:
 def test_sentence_with_terminal() -> None:
     """Test text with terminal punctuation marks."""
     text_with_terminal: str = 'aaa... a, aa'
+    text_with_terminal_expected: str = ''.join((
+        r'{"text":"aaa...","done":false}\n',
+        r'{"text":"a,","done":false}\n',
+        r'{"text":"aa","done":false}\n',
+        done_true,
+    ))
+
     check(
         test_client,
         text_with_terminal,
-        r'{"text":"aaa...","done":false}\n{"text":"Done","done":true}\n',
+        text_with_terminal_expected,
     )
 
 
 def test_sentence_with_internal() -> None:
     """Test text with internal punctuation marks."""
     text_with_internal: str = 'aaa, a, aa'
+    text_with_internal_expected: str = ''.join((
+        r'{"text":"aaa, a,","done":false}\n',
+        r'{"text":"aa","done":false}\n',
+        done_true,
+    ))
+
     check(
         test_client,
         text_with_internal,
-        r'{"text":"aaa, a,","done":false}\n{"text":"Done","done":true}\n',
+        text_with_internal_expected,
     )
 
 
 def test_sentence_with_spaces() -> None:
     """Test text with spaces only."""
     text_with_spaces: str = 'aaa a aa'
+    text_with_spaces_expected: str = ''.join((
+        r'{"text":"aaa a","done":false}\n',
+        r'{"text":"aa","done":false}\n',
+        done_true,
+    ))
+
     check(
         test_client,
         text_with_spaces,
-        r'{"text":"aaa a","done":false}\n{"text":"Done","done":true}\n',
+        text_with_spaces_expected,
     )
 
 
 def test_sentence_without_spaces() -> None:
     """Test text without spaces and any punctuation marks."""
     text_without_spaces: str = 'aaaaa'
+    text_without_spaces_expected: str = ''.join((
+        r'{"text":"aaaaa","done":false}\n',
+        done_true,
+    ))
+
     check(
         test_client,
         text_without_spaces,
-        r'{"text":"aaaaa","done":false}\n{"text":"Done","done":true}\n',
+        text_without_spaces_expected,
     )
