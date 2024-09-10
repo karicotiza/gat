e421d8b5538073b921bc66439d4b76280a6dc1aa
Danial Kurtumerov <kur7umerovd@gmail.com>
2024-09-10 12:17:47 +0500

    tests: main package
    Removed type ignore comment, added init file.

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,7 +5,7 @@ from httpx import Response
 from schemathesis import experimental, models
 from schemathesis.specs.openapi import loaders, schemas
 
-from src.main import Settings, app  # type: ignore
+from src.main import Settings, app
 
 experimental.OPEN_API_3_1.enable()
 
